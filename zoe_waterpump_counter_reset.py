cf4f7dc5cf67361cffae4962038515fcf2a6f883
Jodaille <jody.noury@gmail.com>
2025-03-03 23:28:58 +0100

    add diag session + pass 0 value

--- zoe_waterpump_counter_reset.py
+++ zoe_waterpump_counter_reset.py
@@ -87,6 +87,7 @@ class Virginizer(gui.QDialog):
             <li>Put D position</li>
             <li>Press/Stay START until <b>"Remove card"</b> message</li>
             <li>Put P position</li>
+            <li>Keep key card inserted</li>
         </ul>
         """
 
@@ -171,6 +172,15 @@ class Virginizer(gui.QDialog):
         options.main_window.logview.append(value)
         self.table.setItem(2, 1, QTableWidgetItem(value))
 
+    def start_diag_extend_session(self):
+        sds_request = self.evc_ecu.requests[u"StartDiagnosticSession.ExtendedDiagnosticSession"]
+
+        sds_stream = " ".join(sds_request.build_data_stream({}))
+        if options.simulation_mode:
+            print("SdS stream", sds_stream)
+            return
+        options.elm.start_session_can(sds_stream)
+
     def start_diag_session(self):
         sds_request = self.evc_ecu.requests[u"StartDiagnosticSession.Default"]
 
@@ -181,7 +191,7 @@ class Virginizer(gui.QDialog):
         options.elm.start_session_can(sds_stream)
 
     def reset_lowcounter(self):
-        self.start_diag_session()
+        self.start_diag_extend_session()
         self.reset_low_speed_counter()
 
     def reset_middlecounter(self):
@@ -205,43 +215,43 @@ class Virginizer(gui.QDialog):
 
     def reset_timer_DrivWEP(self):
         reset_request = self.evc_ecu.requests[u"DataWrite.($3531) V_Timer_DrivWEP_ON"]
-        request_response = reset_request.send_request()
+        request_response = reset_request.send_request({"($3531) V_Timer_DrivWEP_ON": "0"})
         print(request_response)
         if request_response is not None:
             self.status_check.setText(_("<font color='green'>CLEAR V_Timer_DrivWEP_ON EXECUTED</font>"))
         else:
             self.status_check.setText(_("<font color='red'>CLEAR V_Timer_DrivWEP_ON FAILED</font>"))
-        self.get_timer_DrivWEP_ON()
+        # self.get_timer_DrivWEP_ON()
 
     def reset_low_speed_counter(self):
         reset_request = self.evc_ecu.requests[u"DataWrite.($3349) Time Counter for the driving WEP in Low Speed"]
-        request_response = reset_request.send_request()
+        request_response = reset_request.send_request({"($3349) Time Counter for the driving WEP in Low Speed": 0})
         print(request_response)
         if request_response is not None:
             self.status_check.setText(_("<font color='green'>CLEAR Low EXECUTED</font>"))
         else:
             self.status_check.setText(_("<font color='red'>CLEAR Low FAILED</font>"))
-        self.get_low_speed_counter()
+        # self.get_low_speed_counter()
 
     def reset_middle_speed_counter(self):
         reset_request = self.evc_ecu.requests[u"DataWrite.($334A) Time Counter for the driving WEP in Middle Speed"]
-        request_response = reset_request.send_request()
+        request_response = reset_request.send_request({"($334A) Time Counter for the driving WEP in Middle Speed": 0})
         print(request_response)
         if request_response is not None:
             self.status_check.setText(_("<font color='green'>CLEAR Medium EXECUTED</font>"))
         else:
             self.status_check.setText(_("<font color='red'>CLEAR Medium FAILED</font>"))
-        self.get_medium_speed_counter()
+        # self.get_medium_speed_counter()
 
     def reset_high_speed_counter(self):
         reset_request = self.evc_ecu.requests[u"DataWrite.($334B) Time Counter for the driving WEP in High Speed"]
-        request_response = reset_request.send_request()
+        request_response = reset_request.send_request({"($334B) Time Counter for the driving WEP in High Speed": 0})
         print(request_response)
         if request_response is not None:
             self.status_check.setText(_("<font color='green'>CLEAR HIGH EXECUTED</font>"))
         else:
             self.status_check.setText(_("<font color='red'>CLEAR HIGH FAILED</font>"))
-        self.get_high_speed_counter()
+        # self.get_high_speed_counter()
 
 def plugin_entry():
     v = Virginizer()
